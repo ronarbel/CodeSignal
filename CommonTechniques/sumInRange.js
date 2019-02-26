88f1825d2e05b70a17e851f19915dfdb0dbe986c
Ron Arbel <ronarbel1@gmail.com>
2019-02-26 03:16:20 -0800

    Add prefixSumsArr optimization

--- CommonTechniques/sumInRange.js
+++ CommonTechniques/sumInRange.js
@@ -31,3 +31,39 @@ function sumInRange(nums, queries) {
   let bigNum = Math.pow(10, 9) + 7;
   return (result + bigNum) % bigNum;
 }
+
+
+//-------- slide query sum based on prev query sum -------- //function sumInRange(nums, queries) {
+  let result = 0;
+  for (let i = queries[0][0]; i <= queries[0][1]; i += 1) {
+    result += nums[i];
+  }
+  
+  let querySum = result;
+  let left = queries[0][0];
+  let right = queries[0][1];
+  for (let i = 1; i < queries.length; i += 1) {
+    let newLeft = queries[i][0];
+    let newRight = queries[i][1];
+    while (left > newLeft) {
+      left -= 1;
+      querySum += nums[left];
+    }
+    while (left < newLeft) {
+      querySum -= nums[left];
+      left += 1;
+    }
+    while (right > newRight) {
+      querySum -= nums[right];
+      right -= 1;
+    }
+    while (right < newRight) {
+      right += 1;
+      querySum += nums[right];
+    }
+    result += querySum;
+  }
+
+  let bigNum = Math.pow(10, 9) + 7;
+  return (result + bigNum) % bigNum;
+}
