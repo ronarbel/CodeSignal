df640ae509ed84cdc16d63317e7f582533e8823f
Ron Arbel <ronarbel1@gmail.com>
2019-02-26 02:16:58 -0800

    Add additional solution

--- CommonTechniques/sumInRange.js
+++ CommonTechniques/sumInRange.js
@@ -1,3 +1,4 @@
+// -------- naive solution -------- //
 function sumInRange(nums, queries) {
   let result = 0;
   queries.forEach((query) => {
@@ -6,4 +7,27 @@ function sumInRange(nums, queries) {
     }
   });
   return result;
+}
+
+
+// -------- store seen values -------- //
+function sumInRange(nums, queries) {
+  let result = 0;
+  let seen = {};
+  queries.forEach(query => {
+    let tempSum = 0;
+    let q = JSON.stringify(query)
+    if (seen[q]) {
+      result += seen[q];
+    } else {
+      for (let i = query[0]; i <= query[1]; i += 1) {
+        tempSum += nums[i];
+      }
+      result += tempSum;
+      seen[q] = tempSum;
+    }
+    
+  });
+  let bigNum = Math.pow(10, 9) + 7;
+  return (result + bigNum) % bigNum;
 }
