ffe29e189463ae495ce2af4a2849c6c781830716
Ron Arbel <ronarbel1@gmail.com>
2019-02-26 00:17:12 -0800

    Refactor optimized solution

--- CommonTechniques/findLongestSubArrayBySum.js
+++ CommonTechniques/findLongestSubArrayBySum.js
@@ -1,32 +1,24 @@
-// -------- O(n^2) reducing sliding window -------- //
-function findLongestSubarrayBySum(s, arr) {
-  let win = arr.length;
-  while (win > 0) {
-    let start = 0;
-    while (start + win <= arr.length) {
-      const tempSum = arr.slice(start, start + win).reduce((a, v) => a + v);
-      if (tempSum === s) return [start + 1, start + win];
-      start += 1;
-    }
-    win -= 1;
-  }
-  return [-1];
-}
+// // -------- O(n^2) reducing sliding window -------- //
+// function findLongestSubarrayBySum(s, arr) {
+//   let win = arr.length;
+//   while (win > 0) {
+//     let start = 0;
+//     while (start + win <= arr.length) {
+//       const tempSum = arr.slice(start, start + win).reduce((a, v) => a + v);
+//       if (tempSum === s) return [start + 1, start + win];
+//       start += 1;
+//     }
+//     win -= 1;
+//   }
+//   return [-1];
+// }
 
 // -------- optimized 0(n) solution -------- //
-// Key: return *longest* subarray by sum.
+// Key to pass last tests: return *longest* subarray by sum.
 function findLongestSubarrayBySum(s, arr) {
-  // edgecase of s = 0, arr = [1,2,3,4,0,0,0,0]
-  if (s === 0) {
-    let firstIndex = arr.indexOf(0);
-    let lastIndex = arr.lastIndexOf(0);
-    if (firstIndex === -1) return [-1];
-    return [firstIndex + 1, lastIndex + 1];
-  }
-  
-  let longestSolutionLength = 0;
-  let result = [-1]
-  
+  let subLength = 0;
+  let result;
+
   let tempSum = arr[0];
   let start = 0;
   for (let i = 1; i <= arr.length; i += 1) {
@@ -35,18 +27,41 @@ function findLongestSubarrayBySum(s, arr) {
       tempSum -= arr[start];
       start += 1;
     }
-    
-    // if correct sum and next element isn't 0, try to set new result
-    if (tempSum === s && arr[i] !== 0) {
-      let tempSolutionLength = i - start;
-      if (tempSolutionLength > longestSolutionLength) {
-        longestSolutionLength = tempSolutionLength;
-        result = [start + 1, i];
-      }
+
+    // if correct sum and subarray length > previous solution length, set result
+    if (tempSum === s && i - start > subLength) {
+      result = [start + 1, i];
+      subLength = i - start;
     }
-    
+
     // otherwise add next element to tempSum
     tempSum += arr[i];
   }
-  return result;
+  return result || [-1];
+}
+
+// -------- alt solution -------- //
+function findLongestSubarrayBySum(s, arr) {
+  var current = []
+  var left = 1;
+  var sum = 0
+  var longestVal = 0
+  var longestArr
+  
+  arr.forEach((val, right) => {
+      current.push(val)
+      sum += val
+      
+      while (sum > s) {
+          sum -= current.shift()
+          left += 1
+      }
+       
+      if (sum === s && current.length > longestVal) {
+          longestVal = current.length
+          longestArr = [left, right + 1]
+      }
+  })
+  
+  return longestArr || [-1]
 }
